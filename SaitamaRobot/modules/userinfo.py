957461c4970aaf8da1f17b5be0ca04a1b3afd9fb
Falco-Grice <79639455+Falco-Grice@users.noreply.github.com>
2021-03-14 17:55:19 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/Gabi_Log_Group">?</a>]'.format(
+        text += ' [<a href="https://t.me/Gabi_Support_Log">?</a>]'.format(
             bot.username
         )
 
